0ad5a215bc2e60af5949402678265d6f4566ff04
DevAkky <developerakky@gmail.com>
2023-03-28 21:22:52 +0530

    responsivness done

--- src/components/startUser/User.js
+++ src/components/startUser/User.js
@@ -24,7 +24,7 @@ const User = ({ setUsername }) => {
           Start
         </button>
       </div>
-      <div class="custom-shape-divider-bottom-1680017593">
+      <div className="custom-shape-divider-bottom-1680017593">
         <svg
           data-name="Layer 1"
           xmlns="http://www.w3.org/2000/svg"
